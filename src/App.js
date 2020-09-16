7986072edb5994dae1bd33c01b2993ff3ccbb25f
azezmohammed <muhamadaziz047@gmail.com>
2020-09-16 07:51:48 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CounterClass from "./Component/Exercise-1/CounterClass";
+import CounterFunction from "./Component/Exercise-1/CounterFunction";
+import ListProduct from "./Component/Exercise-2/ListProduct";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <CounterClass />
+      </div>
+      <div>
+        <CounterFunction />
+      </div>
+      <div>
+        <ListProduct />
+      </div>
     </div>
   );
 }
